b8be22ab117e4e822c908e304526b918d76ac8fa
Yahor Yena <yahor.yena+altana@altana.ai>
2026-02-03 12:33:13 +0100

    WIP configs

---
+++ install.sh
@@ -0,0 +1,70 @@
+detect_pm() {
+  if command -v brew >/dev/null 2>&1; then
+    echo "brew"
+  elif command -v apt-get >/dev/null 2>&1; then
+    echo "apt"
+  elif command -v dnf >/dev/null 2>&1; then
+    echo "dnf"
+  elif command -v yum >/dev/null 2>&1; then
+    echo "yum"
+  elif command -v pacman >/dev/null 2>&1; then
+    echo "pacman"
+  else
+    echo "none"
+  fi
+}
+
+PM="$(detect_pm)"
+
+brew_install_packages() {
+  brew install \
+    font-hack-nerd-font \
+    jandedobbeleer/oh-my-posh/oh-my-posh \
+    vivid \
+    lsd \
+    fzf \
+    neofetch \
+    bat \
+    virtualenv
+}
+
+apt_install_packages() {
+  sudo apt-get update
+  sudo apt-get install -y \
+    fonts-hack \
+    vivid \
+    lsd \
+    fzf \
+    neofetch \
+    bat \
+    virtualenv
+}
+
+case "$PM" in
+  brew)
+    brew_install_packages
+    ;;
+  apt)
+    apt_install_packages
+    ;;
+  *)
+    echo "No supported package manager found"
+    ;;
+esac
+
+# Copy to .config
+for config_dir in posh nvim neofetch ghostty zshrc; do
+  cp -rf "$config_dir" ~/.config/
+done
+
+# Install ZSH autocompletion
+mkdir -p ~/.zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions \
+  ~/.zsh/zsh-autosuggestions
+
+# Source from configured .zshrc (only if exists)
+LINE='source ~/.config/zshrc/.zshrc'
+FILE="$HOME/.zshrc"
+touch "$FILE"
+# append only if missing
+grep -qxF "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
---
+++ zshrc/.zshrc
@@ -0,0 +1,26 @@
+# Aliases 
+alias pip=pip3
+alias pip-login='cd $HOME/Projects/monorepo && task pip-login && source pip.env && cd -'
+alias npm-login='cd $HOME/Projects/monorepo && task npm-login && source npm.env && cd -'
+alias ecr-login='cd $HOME/Projects/monorepo && task docker-login-ecr-cache && cd -'
+alias project_root="git rev-parse --show-toplevel"
+alias lzd='lazydocker'
+alias ls="lsd"
+alias cat="bat"
+alias vim='nvim'
+
+# Enable VI Mode
+setopt VI
+
+# Set ls color scheme
+VIVID_COLOR_SCHEME = "molokai"
+export LS_COLORS=$(vivid generate $(VIVID_COLOR_SCHEME))
+
+# Init the posh config, ignore in Iterm
+export POSH_CONFIG_FILE_PATH="$HOME/.config/posh/posh.json"
+if [ "$TERM_PROGRAM" != "Apple_Terminal" ]; then
+eval "$(oh-my-posh init zsh --config $POSH_CONFIG_FILE_PATH)"
+fi
+
+# Source out of autosuggestions
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
